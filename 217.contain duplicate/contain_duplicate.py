e93e4d1acaf2afd60f4937ea19def8d84edaac15
giorgio <giorgio.gristina91@gmail.com>
2025-02-04 09:26:28 +0000

    added Set solutions

--- 217.contain duplicate/contain_duplicate.py
+++ 217.contain duplicate/contain_duplicate.py
@@ -9,6 +9,24 @@ class Solution:
         return False
 
 
+# Set lenght evaluation against nums list
+
+# class Solution:
+# def containsDuplicate(nums: list[int]) -> bool:
+#     return len(nums) != len(set(nums))
+
+
+# Use Set because is more memory efficient
+
+# class Solution:
+#     def containsDuplicate(self, nums: list[int]) -> bool:
+#         seen = set()
+#         for num in nums:
+#             if num in seen:
+#                 return True
+#             seen.add(num)
+#         return False
+
 solution = Solution()
 
 print(f"there are duplicate {solution.containsDuplicate([1,2,3,1])}")
