e7c7a480cf8920834528541691c5533c8ac91c8e
giorgio <giorgio.gristina91@gmail.com>
2025-02-04 10:48:40 +0000

    clean up the solution using only one Dict and adding the first string in the dict and subtructing the second string char if exist and value greater than 0.

--- 242.valid_anagram/valid_anagram.py
+++ 242.valid_anagram/valid_anagram.py
@@ -5,23 +5,37 @@ class Solution:
 
 # better solution with O(n) TC
 
-        dictT ={}
-        for c in t:
-            if c in dictT:
-                dictT[c] += 1 
-            else:
-             dictT[c] = 1 
-        print(f"T dic is : {dictT}")
-        dictS= {}
+        # dictT ={}
+        # for c in t:
+        #     if c in dictT:
+        #         dictT[c] += 1 
+        #     else:
+        #      dictT[c] = 1 
+        # print(f"T dic is : {dictT}")
+        # dictS= {}
+        # for c in s:
+        #     if c in dictS:
+        #         dictS[c] += 1 
+        #     else: 
+        #         dictS[c] = 1 
+        # print(f"S dic is : {dictS}")
+
+        # return dictT == dictS
+
+
+# cleaned up solution avoiding using the 2 dictionary
+
+        charCount = {}
+
         for c in s:
-            if c in dictS:
-                dictS[c] += 1 
-            else: 
-                dictS[c] = 1 
-        print(f"S dic is : {dictS}")
+            charCount[c] = charCount.get(c, 0) + 1
 
-        return dictT == dictS
-
+        for c in t:
+            if charCount.get(c, 0) > 0:
+                charCount[c] -= 0
+            else:
+                return False
+        return True
 
 # sorting solution less efficient with n log(n) TC
 
@@ -30,3 +44,9 @@ class Solution:
 
         # return sortedSString == sortedTString 
 
+instance = Solution()
+# s = "anagram", t = "anagram" 
+print(f"is an anagram: {instance.isAnagram('anagram',  'anagram')}")
+
+# s = "rat", t = "car"
+print(f"is an anagram: {instance.isAnagram('rat',  'car')}")
