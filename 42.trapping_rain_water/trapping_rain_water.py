548aba8358ee253ab7bf84a1442efbc500c54b21
giorgio <giorgio.gristina91@gmail.com>
2025-03-24 13:38:43 +0000

    trapping rain water solve it usin 2 list which reppressent the left max val and the right max val. once I find them I check with one between the 2  left and right max val at the same index has the lower value and after subtrack the height of the value from height input at the same index.

---
+++ 42.trapping_rain_water/trapping_rain_water.py
@@ -0,0 +1,44 @@
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        res = 0 
+        length = len(height)
+        max_left = [0] * length
+        max_right = [0] * length
+        max_l = 0
+        max_r = 0
+        for i in range(0, length):
+            if i == 0:
+                max_left[i] = 0
+            else:
+                max_left[i] = max(max_left[i - 1], height[i - 1])
+
+        for i in range(length -1, -1, -1):
+            if i == length - 1:
+                max_right[i] = 0
+            else:
+                max_right[i] = max(max_right[i + 1], height[i + 1])
+
+        for i in range(0, length):
+            amount = min(max_left[i], max_right[i]) - height[i]
+            if amount < 0:
+                res += 0
+            else:
+                res += amount
+        return res
+
+
+
+
+
+
+
+solution = Solution()
+
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+print(solution.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
+
+print(solution.trap([4,2,0,3,2,5]))
