b7b60a7e9323f42a4c3c354b9980b08cc241655a
Giorgio Gristina <giorgio.gristina91@gmail.com>
2025-03-31 10:49:38 +0100

    min_stack question, I have solved it as request in O(1) TC keepind track of the minimum element in a min val stack. Therefore, everytime you push an elemnt into the stack u need to check if it is lower than the current lowest and add it to the min val stack. while, when you are popping need to pop also the min val stack.

--- 155.min_stack/min_stack.py
+++ 155.min_stack/min_stack.py
@@ -1,18 +1,32 @@
 class MinStack:
 
     def __init__(self):
-
+        self.stack = []
+        self.min = None
+        self.min_stack = []
 
     def push(self, val: int) -> None:
+        self.stack.append(val)
 
+        if self.min and self.min_stack:
+            if val < self.min:
+                self.min_stack.append(val)
+                self.min = val
+            else:
+                self.min_stack.append(self.min)
+        else:
+            self.min = val
+            self.min_stack.append(val)
 
     def pop(self) -> None:
-
+        self.stack.remove(self.stack[-1])
+        self.min_stack.remove(self.min_stack[-1])
 
     def top(self) -> int:
-
+        return self.stack[-1]
 
     def getMin(self) -> int:
+        return self.min_stack[-1]
 
 
 
@@ -22,3 +36,12 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+obj = MinStack()
+print(obj.push(-2))
+print(obj.push(0))
+print(obj.push(-3))
+print(obj.getMin())
+print(obj.pop())
+print(obj.top())
+print(obj.getMin())
