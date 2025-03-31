ff2d692ab091992ddd15d48424bf0ec4647e4102
Giorgio Gristina <giorgio.gristina91@gmail.com>
2025-03-31 09:00:52 +0100

    trapping rain water with SC O(1) solution without using extra memory to track the previous max left and right

--- 42.trapping_rain_water/trapping_rain_water.py
+++ 42.trapping_rain_water/trapping_rain_water.py
@@ -1,29 +1,53 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        res = 0 
-        length = len(height)
-        max_left = [0] * length
-        max_right = [0] * length
-        max_l = 0
-        max_r = 0
-        for i in range(0, length):
-            if i == 0:
-                max_left[i] = 0
-            else:
-                max_left[i] = max(max_left[i - 1], height[i - 1])
 
-        for i in range(length -1, -1, -1):
-            if i == length - 1:
-                max_right[i] = 0
-            else:
-                max_right[i] = max(max_right[i + 1], height[i + 1])
+        # Solution with TC O(n) SC O(n)
 
-        for i in range(0, length):
-            amount = min(max_left[i], max_right[i]) - height[i]
-            if amount < 0:
-                res += 0
+        # res = 0
+        # length = len(height)
+        # max_left = [0] * length
+        # max_right = [0] * length
+        # max_l = 0
+        # max_r = 0
+        # for i in range(0, length):
+        #     if i == 0:
+        #         max_left[i] = 0
+        #     else:
+        #         max_left[i] = max(max_left[i - 1], height[i - 1])
+
+        # for i in range(length -1, -1, -1):
+        #     if i == length - 1:
+        #         max_right[i] = 0
+        #     else:
+        #         max_right[i] = max(max_right[i + 1], height[i + 1])
+
+        # for i in range(0, length):
+        #     amount = min(max_left[i], max_right[i]) - height[i]
+        #     if amount < 0:
+        #         res += 0
+        #     else:
+        #         res += amount
+        # return res
+
+# Solution with TC O(n) SC O(1)
+
+        res = 0
+        l = 0
+        r = len(height) -1
+        max_l = height[l]
+        max_r = height[r]
+
+        while l < r:
+            if max_l < max_r:
+                l += 1
+                max_l = max(max_l, height[l])
+                res += max_l - height[l]
             else:
-                res += amount
+                r -=1
+                max_r = max(max_r, height[r])
+
+                res += max_r - height[r]
+
         return res
 
 
